af69bfbbfeea048381f8216dc2990b1fddc83bbb
Nadiia Urazmanova <nadegdaurazmanova@gmai.com>
2025-07-27 15:54:15 +0300

    add two pages

---
+++ src/js/2-form.js
@@ -0,0 +1,42 @@
+const formData = {
+  email: "",
+  message: "",
+};
+
+const form = document.querySelector(".feedback-form");
+
+function saveToLocalStorage() {
+  localStorage.setItem("feedback-form-state", JSON.stringify(formData));
+}
+
+window.addEventListener("DOMContentLoaded", () => {
+  const savedData = localStorage.getItem("feedback-form-state");
+  if (savedData) {
+    const parsedData = JSON.parse(savedData);
+    formData.email = parsedData.email || "";
+    formData.message = parsedData.message || "";
+    form.elements.email.value = formData.email;
+    form.elements.message.value = formData.message;
+  }
+});
+
+form.addEventListener("input", (event) => {
+  const target = event.target;
+  if (target.name === "email" || target.name === "message") {
+    formData[target.name] = target.value;
+    saveToLocalStorage();
+  }
+});
+
+form.addEventListener("submit", (event) => {
+  event.preventDefault();
+  if (formData.email.trim() === "" || formData.message.trim() === "") {
+    alert("Fill please all fields");
+    return;
+  }
+  console.log(formData);
+  localStorage.removeItem("feedback-form-state");
+  formData.email = "";
+  formData.message = "";
+  form.reset();
+});
